20b57cb423142da1dea51c0cb18d1de62ccb79ca
anani <abood.anani57@gmail.com>
2022-11-09 21:44:13 +0200

    initial commit

---
+++ loginregestration/src/main/java/com/axsos/loginregestration/LoginregestrationApplication.java
@@ -0,0 +1,13 @@
+package com.axsos.loginregestration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginregestrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginregestrationApplication.class, args);
+	}
+
+}
